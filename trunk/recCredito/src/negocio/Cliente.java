a276d98f3594d252246d02513f4a297a5ef58fbf
iuri.brito <iuri.brito@b83d6458-2250-11de-a7a1-25a91d9625e5>
2009-05-05 00:42:49 +0000

    Teste de inserção

--- trunk/recCredito/src/negocio/Cliente.java
+++ trunk/recCredito/src/negocio/Cliente.java
@@ -10,5 +10,19 @@ package negocio;
  * @author iuri.brito
  */
 public class Cliente {
+    private String nomeEmpresa;
+    private String endereco;
+    private String telefone1;
+    private String telefone2;
+    private String email;
+    private String nomeResponsavel;
 
+    public Cliente(String empresa, String endereco, String telefone1, String telefone2, String email, String responsavel){
+        this.nomeEmpresa = empresa;
+        this.endereco = endereco;
+        this.telefone1 = telefone1;
+        this.telefone2 = telefone2;
+        this.email = email;
+        this.nomeResponsavel = responsavel;
+    }
 }
