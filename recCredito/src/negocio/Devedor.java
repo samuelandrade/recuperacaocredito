590b0afdca42a32c3086bb6f8e05eabd02263ca9
iuri.brito <iuri.brito@b83d6458-2250-11de-a7a1-25a91d9625e5>
2009-05-05 00:48:56 +0000

    Classes Bases motadas

--- recCredito/src/negocio/Devedor.java
+++ recCredito/src/negocio/Devedor.java
@@ -10,17 +10,19 @@ package negocio;
  * @author iuri.brito
  */
 public class Devedor {
-       String nome;
-       String endereco;
-       String telefone1;
-       String telefone2;
-       double valorDebito;
+       private String nome;
+       private String endereco;
+       private String telefone1;
+       private String telefone2;
+       private char status;
+       private double valorDebito;
 
        public Devedor(String nome, String endereco, String telefone1, String telefone2, double debito){
            this.nome = nome;
            this.endereco = endereco;
            this.telefone1 = telefone1;
            this.telefone2 = telefone2;
+           this.status = 'A';
            this.valorDebito = debito;
        }
 }
