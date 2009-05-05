37333286f51c6804dee076b5a5330f7eb8dd5ce0
iuri.brito <iuri.brito@b83d6458-2250-11de-a7a1-25a91d9625e5>
2009-05-05 00:42:49 +0000

    Teste de inserção

--- recCredito/src/negocio/Devedor.java
+++ recCredito/src/negocio/Devedor.java
@@ -12,8 +12,15 @@ package negocio;
 public class Devedor {
        String nome;
        String endereco;
-       String telefone;
-       double debito;
+       String telefone1;
+       String telefone2;
+       double valorDebito;
 
-
+       public Devedor(String nome, String endereco, String telefone1, String telefone2, double debito){
+           this.nome = nome;
+           this.endereco = endereco;
+           this.telefone1 = telefone1;
+           this.telefone2 = telefone2;
+           this.valorDebito = debito;
+       }
 }
