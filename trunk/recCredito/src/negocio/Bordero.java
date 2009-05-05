437f189ee0451f2eed5c833cc7c650baab4f9d9f
iuri.brito <iuri.brito@b83d6458-2250-11de-a7a1-25a91d9625e5>
2009-05-05 00:48:56 +0000

    Classes Bases motadas

---
+++ trunk/recCredito/src/negocio/Bordero.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package negocio;
+
+/**
+ *
+ * @author 20081169212
+ */
+public class Bordero {
+    private int codCliente;
+    private int codDevedor;
+    private int formaPagamento;
+}
